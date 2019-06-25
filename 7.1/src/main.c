a0e46bf3c355f76a2f6edbbe7efdbbdbc127faa8
alejandrotrenado <alejandro.trenado.diaz@alumnos.upm.es>
2019-06-26 00:42:27 +0200

    Add files via upload

---
+++ 7.1/src/main.c
@@ -0,0 +1,219 @@
+#include "esp_common.h"
+#include "freertos/task.h"
+#include "gpio.h"
+#include "fsm.h"
+#define LED 2
+#define PERIOD_TICK 100/portTICK_RATE_MS
+#define tiempoGuarda 100/portTICK_RATE_MS
+#define segundo 1000/portTICK_RATE_MS
+
+volatile int timeout = 0;
+int cuenta=0;
+int timeout_seg=0;
+int correcto=0;
+int CONTR_0=1;
+int CONTR_1=2;
+int CONTR_2=3;
+int aux=0;
+
+/******************************************************************************
+ * FunctionName : user_rf_cal_sector_set
+ * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
+ *                We add this function to force users to set rf cal sector, since
+ *                we don't know which sector is free in user's application.
+ *                sector map for last several sectors : ABCCC
+ *                A : rf cal
+ *                B : rf init data
+ *                C : sdk parameters
+ * Parameters   : none
+ * Returns      : rf cal sector
+*******************************************************************************/
+
+enum fsm_state {
+IDLE,
+CIFRA0,
+CIFRA1,
+CIFRA2,
+APAGADO,
+ENCENDIDO,
+};
+
+
+uint32 user_rf_cal_sector_set(void)
+{
+    flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 5;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
+/*funciones de comprobación */
+
+int cont_correcta(){
+  if(correcto==1){
+    return 1;
+  }
+  return 0;
+}
+int cont_incorrecta(){
+  if(correcto==0){
+    return 1;
+  }
+  return 0;
+}
+
+int tocar(fsm_t *this){
+    if(!GPIO_INPUT_GET(12) ){
+    if (xTaskGetTickCount () > timeout) {
+  		timeout = xTaskGetTickCount () + tiempoGuarda ;
+  		return 1;
+  	}
+    return 0;
+  }
+  return 0;
+
+}
+
+int timeout_1s(){
+  if(xTaskGetTickCount()>=timeout_seg){
+    GPIO_OUTPUT_SET(LED, 0);
+    vTaskDelay(10);
+    GPIO_OUTPUT_SET(LED, 1);
+    return 1;
+  }
+  if(!GPIO_INPUT_GET(12) ){
+    if (xTaskGetTickCount () > timeout) {
+      timeout = xTaskGetTickCount () + tiempoGuarda ;
+      timeout_seg=xTaskGetTickCount()+segundo;
+  		cuenta++;
+  	}
+  }
+
+return 0;
+}
+
+
+/* funciones de salida */
+void inicializar(){
+  aux=0;
+  cuenta=0;
+  correcto=0;
+  timeout_seg=xTaskGetTickCount()+segundo;
+  GPIO_OUTPUT_SET(LED, 1);
+}
+void comprobar_cifra_0(){
+  if(cuenta==10){
+    cuenta=0;
+  }
+  if (cuenta== CONTR_0){
+    aux=1;
+  }
+  else{
+    aux=0;
+  }
+  cuenta=0;
+  timeout_seg=xTaskGetTickCount()+segundo;
+}
+void comprobar_cifra_1(){
+  if(cuenta==10){
+    cuenta=0;
+  }
+  if(cuenta!= CONTR_1 || aux==0){
+    aux=0;
+  }
+  cuenta=0;
+  timeout_seg=xTaskGetTickCount()+segundo;  
+}
+void comprobar_cifra_2(){
+  if(cuenta==10){
+    cuenta=0;
+  }
+  if(cuenta!= CONTR_2 || aux==0){
+    aux=0;
+  }
+  if(aux==1){
+    correcto=1;
+  }
+  cuenta=0;  
+  timeout_seg=xTaskGetTickCount()+segundo;
+}
+
+void LED_ON(){
+  GPIO_OUTPUT_SET(LED, 0);
+}
+
+void LED_OFF(){
+  GPIO_OUTPUT_SET(LED, 1);
+
+}
+/*
+ * Máquina de estados: lista de transiciones
+ * { EstadoOrigen, CondicionDeDisparo, EstadoFinal, AccionesSiTransicion }
+ */
+static fsm_trans_t cifrado[] = {
+  { IDLE, tocar , CIFRA0,  inicializar },
+  { CIFRA0, timeout_1s , CIFRA1,  comprobar_cifra_0 },
+  { CIFRA1, timeout_1s , CIFRA2,  comprobar_cifra_1 },
+  { CIFRA2, timeout_1s , IDLE,  comprobar_cifra_2 },
+  {-1, NULL, -1, NULL },
+};
+static fsm_trans_t encendido[]={
+  {APAGADO,cont_correcta,ENCENDIDO,LED_ON},
+  {ENCENDIDO,cont_incorrecta,APAGADO,LED_OFF},
+  {-1,NULL,-1,NULL},
+};
+
+
+
+void sensor(void* ignore)
+{   
+    fsm_t* fsm_1 = fsm_new(cifrado);
+    fsm_t* fsm_2 = fsm_new(encendido);
+    portTickType xLastWakeTime;
+
+    xLastWakeTime = xTaskGetTickCount ();
+    while(true) {
+      fsm_fire(fsm_1);
+      fsm_fire(fsm_2);
+
+      vTaskDelayUntil(&xLastWakeTime, PERIOD_TICK);
+    }
+
+    
+}
+
+/******************************************************************************
+ * FunctionName : user_init
+ * Description  : entry of user application, init user function here
+ * Parameters   : none
+ * Returns      : none
+*******************************************************************************/
+void user_init(void)
+{
+    PIN_FUNC_SELECT(GPIO_PIN_REG_12,FUNC_GPIO12);
+    xTaskCreate(&sensor, "startup", 2048, NULL, 1, NULL);
+}
+
